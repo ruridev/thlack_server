3aeba8a2918e2acfd509379e11a9a338d5e1bc04
ruridev <devruri@gmail.com>
2020-09-03 01:37:30 +0900

    Workspace 모델 추가

---
+++ app/models/workspace_owner.rb
@@ -0,0 +1,4 @@
+class WorkspaceOwner < ApplicationRecord
+  belongs_to :user
+  belongs_to :workspace
+end
---
+++ spec/factories/workspace_owners.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :workspace_owner do
+    user { nil }
+    workspace { nil }
+  end
+end
