91810ca8c18a94a600b5cc155dc05771b6b14a79
ruridev <devruri@gmail.com>
2020-09-08 22:25:54 +0900

    Account User Assosiation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,9 @@
 class Account < ApplicationRecord
   has_secure_password
 
+  has_many :account_users
+  has_many :users, through: :account_users
+
   validates :identifier, presence: true
   validates :type, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_one :account_user
+  has_one :account, through: :account_user
 end
