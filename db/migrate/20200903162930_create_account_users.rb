b95081c4d80fe7ce68d42c6d90bbc16fadbade4e
ruridev <devruri@gmail.com>
2020-09-08 22:26:11 +0900

    :wrench: migrate AccountUsers

--- db/migrate/20200903162930_create_account_users.rb
+++ db/migrate/20200903162930_create_account_users.rb
@@ -1,6 +1,6 @@
-class CreateAccountUser < ActiveRecord::Migration[6.0]
+class CreateAccountUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :account_user do |t|
+    create_table :account_users do |t|
       t.references :user, null: false, foreign_key: true
       t.references :account, null: false, foreign_key: true
 
